8d47d8d6b73ae30591f035f0b8e0d25d8a53f4c8
Sailesh <saileshrokaya123@gmail.com>
2021-06-28 22:38:39 +0545

    This is my first commit

---
+++ WebBrowserController/WebBrowserController/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WebBrowserController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnFrd = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter URL :";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(145, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(486, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Image = global::WebBrowserController.Properties.Resources.Print1;
+            this.btnPrint.Location = new System.Drawing.Point(829, 10);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(42, 41);
+            this.btnPrint.TabIndex = 7;
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = global::WebBrowserController.Properties.Resources.Home1;
+            this.btnHome.Location = new System.Drawing.Point(3, 7);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(53, 51);
+            this.btnHome.TabIndex = 6;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = global::WebBrowserController.Properties.Resources.Backward1;
+            this.btnBack.Location = new System.Drawing.Point(685, 10);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(42, 41);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnFrd
+            // 
+            this.btnFrd.Image = global::WebBrowserController.Properties.Resources.Forward1;
+            this.btnFrd.Location = new System.Drawing.Point(733, 10);
+            this.btnFrd.Name = "btnFrd";
+            this.btnFrd.Size = new System.Drawing.Size(42, 41);
+            this.btnFrd.TabIndex = 4;
+            this.btnFrd.UseVisualStyleBackColor = true;
+            this.btnFrd.Click += new System.EventHandler(this.btnFrd_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Image = global::WebBrowserController.Properties.Resources.Preview1;
+            this.btnPreview.Location = new System.Drawing.Point(781, 10);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(42, 41);
+            this.btnPreview.TabIndex = 3;
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Image = global::WebBrowserController.Properties.Resources.search;
+            this.btnSearch.Location = new System.Drawing.Point(637, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(42, 41);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.btnPrint);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnFrd);
+            this.panel1.Controls.Add(this.btnPreview);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(892, 65);
+            this.panel1.TabIndex = 8;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(2, 80);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(891, 595);
+            this.webBrowser.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 681);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnFrd;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ WebBrowserController/WebBrowserController/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowserController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void btnFrd_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(txtSearch.Text);
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            webBrowser.ShowPrintDialog();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            webBrowser.ShowPrintPreviewDialog();
+        }
+    }
+}
